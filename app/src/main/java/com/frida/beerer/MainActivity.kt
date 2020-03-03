b8f7a0c2f5fd33a476194458fa47b14c49ed4ba1
Mihajlo Jovanovic <mihajlo.jovanovic@boutsourcing.com>
2020-03-03 16:59:42 +0100

    Implement Profile Android Page FrontEnd - initial commit

--- app/src/main/java/com/frida/beerer/MainActivity.kt
+++ app/src/main/java/com/frida/beerer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.frida.beerer
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -32,7 +33,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when(item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                this.startActivity(intent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
---
+++ app/src/main/java/com/frida/beerer/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.frida.beerer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
