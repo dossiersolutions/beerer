8f0c9d6a46f6f0708042a8965f017bec062b8335
Milos Ignjatovic <milos.ignjatovic@boutsourcing.com>
2020-03-17 13:19:59 +0100

    BeerDetails - example api request

--- app/src/main/java/com/frida/beerer/activities/BeerDetailActivity.kt
+++ app/src/main/java/com/frida/beerer/activities/BeerDetailActivity.kt
@@ -5,10 +5,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.frida.beerer.R
 import com.frida.beerer.domain.Beer
+import okhttp3.*
+import java.io.IOException
 
 const val INTENT_EXTRA_BEER_KEY = "INTENT_EXTRA_BEER_KEY"
 
 class BeerDetailActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
+    private val exampleDetailUrl = "https://api.punkapi.com/v2/beers/1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,8 @@ class BeerDetailActivity : AppCompatActivity() {
 
         initActivity(beer)
         Toast.makeText(this, beer.description, Toast.LENGTH_SHORT).show()
+
+        run(exampleDetailUrl)
     }
 
     private fun initActivity(beer: Beer) {
@@ -28,5 +36,20 @@ class BeerDetailActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("ERROR")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                println(response.body()?.string())
+            }
+        })
     }
 }
